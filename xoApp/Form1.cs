2f661068eb8c4b32d280457d4af26a27186c2619
Savit <tonsavit12345@gmail.com>
2024-08-19 15:43:07 +0700

    1.0

--- xoApp/Form1.cs
+++ xoApp/Form1.cs
@@ -14,6 +14,8 @@ namespace xoApp
     {
         Player currentPlayer;
 
+        int turn = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +27,76 @@ namespace xoApp
             O
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            currentPlayer = Player.X;
+            turn++;
+        }
 
+        private void buttonClick(object sender)
+        {
+            var button = (Button)sender;
+            if(currentPlayer == Player.X)
+            {
+                currentPlayer = Player.X;
+                button.Text = currentPlayer.ToString();
+                button.Enabled = false;
+                button.BackColor = Color.LightGreen;
+            }
+            else
+            {
+                currentPlayer = Player.O;
+                button.Text = currentPlayer.ToString();
+                button.Enabled = false;
+                button.BackColor = Color.LightSalmon;
+            }
+
+            turn++;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
     }
 }
